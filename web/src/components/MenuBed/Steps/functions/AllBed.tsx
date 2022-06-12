0d55b16dea1b6e5b3181a1e75a5ccaefbf56a00c
thyago nunes <devthyagonunes@gmail.com>
2022-06-11 22:53:25 -0300

    Amanhã eu vejo todos esses rabiscos e finalizo

--- web/src/components/MenuBed/Steps/functions/AllBed.tsx
+++ web/src/components/MenuBed/Steps/functions/AllBed.tsx
@@ -15,36 +15,41 @@ export function AllBed() {
         /* console.log(response.data) */
         setBedsVacant(response.data)
       })
-      const convertido = Object.entries(setBedsVacant).map(([key, values]) => {
-        return  (
-          [key, values]
-        )
+
+      const convertido = Object.entries(bedsVacant).map(([key, values]) => {
+       return ([key, values])
       })
 
+
+      console.log(typeof(convertido))
       
 
   return (
     <>
 
-
-
-    
-     
+      {convertido.map(([key, values]) => {
+        return (
+          <li
+            key={key}
+          >
+            {[,values]}
+          </li>
+        )
+      })}
       <form className="relative top-12 my-4 w-full text-center flex flex-row justify-center" >
         <select className="absolute w-96 h[120] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none" 
         >
         
-        <option>
-   {Object.entries(convertido).map(([key, values]) => {
+{/*         { convertido.map(([key, values ]) => {
         return (
           <option
             key={key}
           >
-            {[values]}
+            {values}
           </option> 
         )
       })}
-   </option>
+ */}
          
         </select>
        
