a74431fa84b021b438f293150429f9cf3937ff64
thyago nunes <devthyagonunes@gmail.com>
2022-06-13 23:14:22 -0300

    one more day ...

--- web/src/components/MenuBed/Steps/functions/AllBed.tsx
+++ web/src/components/MenuBed/Steps/functions/AllBed.tsx
@@ -1,51 +1,91 @@
 import { useEffect, useState } from "react";
 import { api } from "../../../../lib/api"
 
- interface BedsVacant {
-  id: string,
-  name: string ,
-  patientId: string,
-} 
+
 
 export function AllBed() {
-  let [bedsVacant, setBedsVacant] = useState<BedsVacant[]>([
-    { id: '1234abcd', name: 'thyago nunes', patientId: '1234abcd' },
-  ])
+interface BedsVacant {
+  id: string, 
+  name: string, 
+  patientId: string
+}
+
+  const [detail, setDetail] = useState<BedsVacant[]>([]);
+
+  const RecipeDetail = async () => {
+    const res = await api('/leitos/vagos');
+    Object.entries(res.data).map((value) => {
+      return (
+        value[1]
+      )
+    })
+    console.log(res.data)
+
+    setDetail(res.data);
+    console.log(res.data);
+  };
 
   useEffect(() => {
-    api.get('/leitos/vagos')
-      .then(response => {
-        setBedsVacant(response.data)
-        return setBedsVacant
-        
-      })
-  }, [])
+    RecipeDetail();
+  }, [detail]);
 
-/*   const results = Object.keys(bedsVacant).map(([key, values]) => {
-    return [key, values]
-  }) */
-  console.log(bedsVacant);
-  Object.keys(bedsVacant)
+  console.log(`Hi ${detail}`);
+
 
   return (
     <>
+      
+      { 
+        Object.entries(detail)?.map((values) => { 
+        return (
+   
+          <li 
+            key={values[1].id}
+            value={values[1].name}
+          >
+            {values[1].name}
+          </li>
+          
+        )
+      })} 
       <form className="relative top-12 my-4 w-full text-center flex flex-row justify-center" >
-        <select className="absolute w-96 h[120] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"
+        <select
+          className="absolute w-96 h[120] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"
         >
+         { 
+        Object.entries(detail)?.map((values) => { 
+        return (
+         <>
+          <option 
+            key={values[1].id}
+            value={values[1].name}
+          >
+            {values[1].name}
+          </option>
+          
 
-    {Object.entries(bedsVacant).map(([key, values]) => {
-      return (
-        <option 
-        key={key}
-        value={[,values.name]}>
-          {values.name}
-        </option>
-      )
-    })}
+          <option 
+            key={values[1].id}
+            value={values[1].name}
+          >
+            {values[1].name}
+          </option>
+          
+
+           <option 
+            key={values[1].id}
+            value={values[1].name}
+          >
+            {values[1].name}
+          </option>
+          
+         </>
+        )
+      })} 
         </select>
-
-        <br /><br />
       </form>
     </>
   )
 }
+
+
